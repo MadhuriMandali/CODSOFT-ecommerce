807cb9e70db77e6c674a9335dea26cedba23ab6d
mandalimadhuri08@gmail.com <mandalimadhuri082gmail.com>
2026-06-08 03:29:12 -0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const productRoutes = require('./routes/productRoutes');
+const authRoutes = require('./routes/authRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+app.use('/api/products', productRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/orders', orderRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
